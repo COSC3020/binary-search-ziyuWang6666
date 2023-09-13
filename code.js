d003587320a9800f38aeedf0758c2b94b22d9a91
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-09-13 13:42:09 -0600

    Update code.js
    It seems like the program won't run on my computer because of a stack overflow error, but I don't know how to fix it.

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function binarySearch(list, element) {
-    return -1;
+    list.sort();
+    var start = 0;
+    var end = list.length-1;
+    var mid = Math.floor((start+end)/2);
+
+    while(start <= end) {
+        if(list[mid]===element){
+            return true;
+        } else if(list[mid] > element) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return false;
+    //return -1;
 }
+result = binarySearch([1,2,3,4,5,6,7], 5);
+console.log(result);
+
+// get help from TA: Ali
+//https://www.educative.io/answers/how-to-check-if-a-number-is-odd-or-even-in-javascript
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
