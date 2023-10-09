7e0ccf68cd30f645dfa9a344a5150009039f79dc
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-10-08 20:15:36 -0600

    Update code.js
    change sort

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function binarySearch(list, element) {
     if(list.length<1) return -1;//when list = [], return -1
-    list.sort();
+    list.sort(function(a, b){return a - b});
     var start = 0;
     var end = list.length-1;
-    var mid = Math.floor((start+end)/2);
+    var mid;
 
     while(start <= end) {
         //always recursive in while need reset mid, otherwise mid will always the same number
@@ -19,9 +19,7 @@ function binarySearch(list, element) {
 
     return -1;
 }
-result = binarySearch([1,2,3,4,5,6,7], 5);
-console.log(result);
-
+binarySearch([1, 10, 31], 1);
 // get help from TA: Ali
 //https://www.educative.io/answers/how-to-check-if-a-number-is-odd-or-even-in-javascript
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
