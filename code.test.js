6c5243dc746997edd1706777aa30d542946f8659
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-10-08 20:16:48 -0600

    Update code.test.js
    make test.js run

--- code.test.js
+++ code.test.js
@@ -7,10 +7,11 @@ const testSearch =
     jsc.forall("array nat", function(arr) {
         if(arr.length > 0) {
             arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0;
+            return binarySearch(arr, arr[0]) === 0|| arr[binarySearch(arr,arr[0])]==arr[0];
         } else {
             return binarySearch(arr, "foo") === -1;
         }
     });
 
 jsc.assert(testSearch);
+//get some reference from Dhruv: arr[binarySearch(arr,arr[0])]==arr[0]
