570409f254d18e0b26ae2ca55eab9067dc62068b
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-10-11 14:35:10 -0600

    Update code.js
    remove sort list in binarySearch function

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 function binarySearch(list, element) {
     if(list.length<1) return -1;//when list = [], return -1
-    list.sort(function(a, b){return a - b});
     var start = 0;
     var end = list.length-1;
     var mid;
@@ -20,6 +19,8 @@ function binarySearch(list, element) {
     return -1;
 }
 binarySearch([1, 10, 31], 1);
+// result = binarySearch([-1, 10, 31], 1);
+// console.log(result);
 // get help from TA: Ali
 //https://www.educative.io/answers/how-to-check-if-a-number-is-odd-or-even-in-javascript
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
