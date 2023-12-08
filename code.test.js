c1bba4db1c01c9bb3623bca3c765b6aa7c001fc9
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-07 17:03:21 -0700

    Update code.test.js
    remove error code

--- code.test.js
+++ code.test.js
@@ -7,7 +7,7 @@ const testSearch =
     jsc.forall("array nat", function(arr) {
         if(arr.length > 0) {
             arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0|| arr[binarySearch(arr,arr[0])]==arr[0];
+            return binarySearch(arr, arr[0]) === 0;
         } else {
             return binarySearch(arr, "foo") === -1;
         }
